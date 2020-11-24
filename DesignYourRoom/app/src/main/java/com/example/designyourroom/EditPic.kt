f4734b4b715aaeeba070c45d50ef2a8a725a04bd
Lorena <llorenadenisa@gmail.com>
2020-11-24 20:23:35 +0200

    Color filling wall, doesn't work in EditPic

--- DesignYourRoom/app/src/main/java/com/example/designyourroom/EditPic.kt
+++ DesignYourRoom/app/src/main/java/com/example/designyourroom/EditPic.kt
@@ -1,33 +1,86 @@
 package com.example.designyourroom
 import android.graphics.Bitmap
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 
-class EditPic : AppCompatActivity(){
+import android.util.DisplayMetrics
+import org.opencv.android.Utils
+import org.opencv.core.*
+import org.opencv.imgproc.Imgproc
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.edit_photo_layout)
+class EditPic(bitmap: Bitmap) {
+    val chosenColor = Color.RED
+    var bitmap = bitmap
+     fun rpPaintHSV(bitmap: Bitmap, p: Point): Mat {
+        val cannyMinT = 30.0
+        val ratio = 2.5
 
+        val mRgbMat = Mat()
+        Utils.bitmapToMat(bitmap, mRgbMat)
+        Imgproc.cvtColor(mRgbMat,mRgbMat, Imgproc.COLOR_RGBA2RGB)
+        val mask = Mat(Size(mRgbMat.width()/8.0, mRgbMat.height()/8.0), CvType.CV_8UC1, Scalar(0.0))
+
+        val img = Mat()
+        mRgbMat.copyTo(img)
+        // grayscale
+        val mGreyScaleMat = Mat()
+        Imgproc.cvtColor(mRgbMat, mGreyScaleMat, Imgproc.COLOR_RGB2GRAY, 3)
+        Imgproc.medianBlur(mGreyScaleMat,mGreyScaleMat,3)
+        val cannyGreyMat = Mat()
+        Imgproc.Canny(mGreyScaleMat, cannyGreyMat, cannyMinT, cannyMinT*ratio, 3)
+        //hsv
+        val hsvImage = Mat()
+        Imgproc.cvtColor(img,hsvImage, Imgproc.COLOR_RGB2HSV)
+        //got the hsv values
+        val list = ArrayList<Mat>(3)
+        Core.split(hsvImage, list)
+        val sChannelMat = Mat()
+        Core.merge(listOf(list.get(1)), sChannelMat)
+        Imgproc.medianBlur(sChannelMat,sChannelMat,3)
+
+        // canny
+        val cannyMat = Mat()
+        Imgproc.Canny(sChannelMat, cannyMat, cannyMinT, cannyMinT*ratio, 3)
+
+        Core.addWeighted(cannyMat,0.5, cannyGreyMat,0.5 ,0.0,cannyMat)
+        Imgproc.dilate(cannyMat, cannyMat,mask, Point(0.0,0.0), 5)
+
+        val displayMetrics = DisplayMetrics()
+        //windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val height = displayMetrics.heightPixels
+        val width = displayMetrics.widthPixels
+        val seedPoint = Point(p.x*(mRgbMat.width()/width.toDouble()), p.y*(mRgbMat.height()/height.toDouble()))
+        Imgproc.resize(cannyMat, cannyMat, Size(cannyMat.width() + 2.0, cannyMat.height() + 2.0))
+        Imgproc.medianBlur(mRgbMat,mRgbMat,15)
+        val floodFillFlag = 8
+
+        Imgproc.floodFill(
+            mRgbMat,
+            cannyMat,
+            seedPoint,
+            Scalar(
+                Color.red(chosenColor).toDouble(),
+                Color.green(chosenColor).toDouble(),
+                Color.blue(chosenColor).toDouble()),
+            Rect(),
+            Scalar(5.0, 5.0, 5.0),
+            Scalar(5.0, 5.0, 5.0),
+            floodFillFlag
+        )
+        // showImage(mRgbMat,floodFillImage)
+        Imgproc.dilate(mRgbMat, mRgbMat, mask, Point(0.0,0.0), 5)
+        //got the hsv of the mask image
+        val rgbHsvImage = Mat()
+        Imgproc.cvtColor(mRgbMat,rgbHsvImage, Imgproc.COLOR_RGB2HSV)
+        val list1 = ArrayList<Mat>(3)
+        Core.split(rgbHsvImage, list1)
+        //merged the “v” of original image with mRgb mat
+        val result = Mat()
+        Core.merge(listOf(list1.get(0),list1.get(1),list.get(2)), result)
+        // converted to rgb
+        Imgproc.cvtColor(result, result, Imgproc.COLOR_HSV2RGB)
+        Core.addWeighted(result,0.7, img,0.3 ,0.0,result )
+        //showImage(result,img_set)
+        return result
     }
-
-    fun processImage(bmp:Bitmap)
-    {
-        val edgeDet = EdgeDetection()
-        edgeDet.detectEdges(bmp)
-
-    }
-
-//    fun checkIfAppSupportsDepthApi()
-//    {
-//        val config = session.config
-//
-//        val isDepthSupported = session.isDepthModeSupported(Config.DepthMode.AUTOMATIC)
-//        if (isDepthSupported) {
-//            config.depthMode = Config.DepthMode.AUTOMATIC
-//        }
-//        session.configure(config)
- //   }
 
 }
--- DesignYourRoom/app/src/main/java/com/example/designyourroom/PhotoMethodSelector.kt
+++ DesignYourRoom/app/src/main/java/com/example/designyourroom/PhotoMethodSelector.kt
@@ -2,12 +2,17 @@ package com.example.designyourroom
 
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
+import android.graphics.Color
 import android.graphics.ImageFormat
 import android.graphics.drawable.BitmapDrawable
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.MenuItem
+import android.view.MotionEvent
+import android.view.View
 import android.widget.ImageView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -15,20 +20,24 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.edit_photo_layout.*
 import org.opencv.android.OpenCVLoader
 import org.opencv.android.Utils
-import org.opencv.core.CvType
-import org.opencv.core.Mat
+import org.opencv.core.*
 import org.opencv.imgproc.Imgproc
 
 
 class PhotoMethodSelector: AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
-
+    lateinit var bitmap: Bitmap
+    lateinit var tl: Point
+    var touchCount = 0
+    var chosenColor = Color.GREEN
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.photo_select_method)
         OpenCVLoader.initDebug();
         val bottomNavigationMenu: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        tl = Point()
+
 
         bottomNavigationMenu.setOnNavigationItemSelectedListener(this)
     }
@@ -55,16 +64,33 @@ class PhotoMethodSelector: AppCompatActivity(), BottomNavigationView.OnNavigatio
     }
 
     @RequiresApi(Build.VERSION_CODES.P)
-    @SuppressLint("UseCompatLoadingForDrawables")
+    @SuppressLint("UseCompatLoadingForDrawables", "ClickableViewAccessibility")
     private fun openGalleryForImage() {
 //        val intent = Intent(Intent.ACTION_PICK).apply{
 //            this.type="image/*"
 //            startActivityForResult(this, REQUEST_CODE)
 //        }
         img_set.setImageResource(R.drawable.room_img)
-        val imageBmp = (img_set.drawable as BitmapDrawable).bitmap
-        detectEdges(imageBmp)
+        bitmap = (img_set.drawable as BitmapDrawable).bitmap
+        val picEditor = EditPic(bitmap)
+        img_set.setOnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                if (touchCount == 0) {
+                    tl.x = event.x.toDouble()
+                    tl.y = event.y.toDouble()
 
+                   rpPaintHSV(bitmap, tl)
+                }
+            }
+            true
+            }
+
+    }
+    private fun showImage(image: Mat, view: ImageView) {
+        val mBitmap = Bitmap.createBitmap(image.cols(), image.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(image, mBitmap)
+        view.setImageBitmap(mBitmap)
+        bitmap = mBitmap
     }
 
     @RequiresApi(Build.VERSION_CODES.P)
@@ -80,33 +106,79 @@ class PhotoMethodSelector: AppCompatActivity(), BottomNavigationView.OnNavigatio
 //            }
 //        }
 //    }
-//    private fun detectEdges(bmp : Bitmap)
-//    {
-//        val rgba = Mat()
-//        Log.d("rgb",rgba.toString())
-//
-//        Utils.bitmapToMat(bmp, rgba)
-//        val edges = Mat(rgba.size(), CvType.CV_8UC1)
-//        Log.d("edges", edges.toString())
-//        Imgproc.cvtColor(rgba, edges, Imgproc.COLOR_RGB2GRAY, 4)
-//        Imgproc.Canny(edges, edges, 80.0, 100.0)
-//
-//        showMat(img_set,edges)
-//
-//    }
-//    private fun showMat(view: ImageView, source: Mat) {
-//        Log.d("showMat", "Am ajuns in functia showMat")
-//        val bitmap = Bitmap.createBitmap(source.width(), source.height(), Bitmap.Config.ARGB_8888)
-//        bitmap.density = 360
-//        Utils.matToBitmap(source, bitmap)
-//        view.setImageBitmap(bitmap)
-//    }
 
-    private fun imgToBitmap(image: ImageFormat):Bitmap
-    {
-        return (img_set.drawable as BitmapDrawable).bitmap
+    fun rpPaintHSV(bitmap: Bitmap, p: Point): Mat {
+        val cannyMinT = 30.0
+        val ratio = 2.5
+
+        val mRgbMat = Mat()
+        Utils.bitmapToMat(bitmap, mRgbMat)
+        Imgproc.cvtColor(mRgbMat,mRgbMat, Imgproc.COLOR_RGBA2RGB)
+        val mask = Mat(Size(mRgbMat.width()/8.0, mRgbMat.height()/8.0), CvType.CV_8UC1, Scalar(0.0))
+
+        val img = Mat()
+        mRgbMat.copyTo(img)
+        // grayscale
+        val mGreyScaleMat = Mat()
+        Imgproc.cvtColor(mRgbMat, mGreyScaleMat, Imgproc.COLOR_RGB2GRAY, 3)
+        Imgproc.medianBlur(mGreyScaleMat,mGreyScaleMat,3)
+        val cannyGreyMat = Mat()
+        Imgproc.Canny(mGreyScaleMat, cannyGreyMat, cannyMinT, cannyMinT*ratio, 3)
+        //hsv
+        val hsvImage = Mat()
+        Imgproc.cvtColor(img,hsvImage, Imgproc.COLOR_RGB2HSV)
+        //got the hsv values
+        val list = ArrayList<Mat>(3)
+        Core.split(hsvImage, list)
+        val sChannelMat = Mat()
+        Core.merge(listOf(list.get(1)), sChannelMat)
+        Imgproc.medianBlur(sChannelMat,sChannelMat,3)
+
+        // canny
+        val cannyMat = Mat()
+        Imgproc.Canny(sChannelMat, cannyMat, cannyMinT, cannyMinT*ratio, 3)
+
+        Core.addWeighted(cannyMat,0.5, cannyGreyMat,0.5 ,0.0,cannyMat)
+        Imgproc.dilate(cannyMat, cannyMat,mask, Point(0.0,0.0), 5)
+
+        val displayMetrics = DisplayMetrics()
+       windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val height = displayMetrics.heightPixels
+        val width = displayMetrics.widthPixels
+        val seedPoint = Point(p.x*(mRgbMat.width()/width.toDouble()), p.y*(mRgbMat.height()/height.toDouble()))
+        Imgproc.resize(cannyMat, cannyMat, Size(cannyMat.width() + 2.0, cannyMat.height() + 2.0))
+        Imgproc.medianBlur(mRgbMat,mRgbMat,15)
+        val floodFillFlag = 8
+
+        Imgproc.floodFill(
+            mRgbMat,
+            cannyMat,
+            seedPoint,
+            Scalar(
+                Color.red(chosenColor).toDouble(),
+                Color.green(chosenColor).toDouble(),
+                Color.blue(chosenColor).toDouble()),
+            Rect(),
+            Scalar(5.0, 5.0, 5.0),
+            Scalar(5.0, 5.0, 5.0),
+            floodFillFlag
+        )
+        // showImage(mRgbMat,floodFillImage)
+        Imgproc.dilate(mRgbMat, mRgbMat, mask, Point(0.0,0.0), 5)
+        //got the hsv of the mask image
+        val rgbHsvImage = Mat()
+        Imgproc.cvtColor(mRgbMat,rgbHsvImage, Imgproc.COLOR_RGB2HSV)
+        val list1 = ArrayList<Mat>(3)
+        Core.split(rgbHsvImage, list1)
+        //merged the “v” of original image with mRgb mat
+        val result = Mat()
+        Core.merge(listOf(list1.get(0),list1.get(1),list.get(2)), result)
+        // converted to rgb
+        Imgproc.cvtColor(result, result, Imgproc.COLOR_HSV2RGB)
+        Core.addWeighted(result,0.7, img,0.3 ,0.0,result )
+        showImage(result,img_set)
+        return result
     }
-
 
 
     companion object {
